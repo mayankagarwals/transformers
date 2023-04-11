e94cca3139b6f9f08eaa6c1468b13372bca7603d
Mayank Agarwal <mayankagarwal44442@gmail.com>
2023-04-11 16:35:01 +0530

    working example of beam generation

--- src/try.py
+++ src/try.py
@@ -1,12 +1,12 @@
 from transformers import TFGPT2LMHeadModel, GPT2LMHeadModel, FlaxGPT2LMHeadModel, GPT2Tokenizer
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-# model = FlaxGPT2LMHeadModel.from_pretrained("gpt2")
-model = TFGPT2LMHeadModel.from_pretrained("gpt2")
+model = FlaxGPT2LMHeadModel.from_pretrained("gpt2")
+# model = TFGPT2LMHeadModel.from_pretrained("gpt2")
 # model = GPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
 
-input_ids = tokenizer.encode('I enjoy walking with my cute dog', return_tensors='tf')
-print(input_ids.shape)
+input_ids = tokenizer.encode('I enjoy walking with my cute dog', return_tensors='jax')
+print(input_ids)
 
 beam_output = model.generate(
     input_ids,
@@ -16,8 +16,8 @@ beam_output = model.generate(
     early_stopping=True
 )
 
-# print(tokenizer.decode(beam_output.sequences.tolist()[0], skip_special_tokens=False))
-print(tokenizer.decode(beam_output[0], skip_special_tokens=True))
+print(tokenizer.decode(beam_output.sequences.tolist()[0], skip_special_tokens=False))
+# print(tokenizer.decode(beam_output[0], skip_special_tokens=True))
 
 # I enjoy walking with my cute dog, but I'm not sure if I'll ever be able to walk with him again.
 # I'm not sure if I'll ever be able to walk with him again. I'm not sure if I'll
